96938b0711440857ece6c41944f707f676e60677
Werner <werner.damboeck@letto.at>
2023-03-27 09:19:22 +0200

    Ping

--- src/test/java/at/ac/htlstp/demo/springbootrestclientopen/tests/Ping.java
+++ src/test/java/at/ac/htlstp/demo/springbootrestclientopen/tests/Ping.java
@@ -3,13 +3,17 @@ package at.ac.htlstp.demo.springbootrestclientopen.tests;
 import at.ac.htlstp.demo.springbootrestclientopen.service.PingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 public class Ping {
 
     @Autowired private PingService pingService;
 
     @Test
     public void testPingGet() {
-        System.out.println(pingService.ping(PingService.pingLocalRestServer));
+        assertTrue(pingService.ping(PingService.pingLocalRestServer));
     }
 }
